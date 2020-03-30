359e0b61335af98fb139e4e342eaf1bcd1606513
problemfactoryfesb <jgrgur01@fesb.hr>
2020-03-30 16:24:55 +0200

    Merged

--- lab1/NewClass.cs
+++ lab1/NewClass.cs
@@ -8,13 +8,13 @@ namespace lab1
 {
     class NewClass
     {
-<<<<<<< HEAD
+
         void metoda()
-        {
-=======
+        { }
+
         void promjena(){
         
->>>>>>> bcd41def7a5050c70b30f601cf2ca8d844c66e0f
+
         }
     }
 }
